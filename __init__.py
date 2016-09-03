bf2e19741645e215bcb00d2ccb66918c9ae83b90
Adrian Letchford <me@dradrian.com>
2016-09-03 11:09:54 +1000

    merged the login process into a single session class

--- __init__.py
+++ __init__.py
@@ -1,41 +1,47 @@
 # -*- coding: utf-8 -*-
 """
 :Author: `Adrian Letchford <http://www.dradrian.com>`_
-:Organisation: `Warwick Business School <http://www.wbs.ac.uk/>`_,
-`University of Warwick <http://www.warwick.ac.uk/>`_.
 :Created On: Tue May 06 13:31:45 2014
 """
 
 from web import GenericLogin
 
-class GoogleLogin(GenericLogin):
+class UnverifiedLoginException(Exception):
+    """
+    Exception for when we could not verify that we've logged into Google."
+    """
+    pass
+
+class Session(GenericLogin):
     """
     A requests Session that is logged into Google's services.
     """
 
-    def _verify(self, resp):
+    def __init__(self, username, password, disp=False):
 
+        url_auth = 'https://accounts.google.com/accounts/ServiceLoginAuth'
+        url_login = 'https://accounts.google.com/ServiceLogin'
+
+        super(Session, self).__init__(username,
+                                      password,
+                                      url_authenticate=url_auth,
+                                      url_login=url_login,
+                                      select_form="@id='gaia_loginform'",
+                                      username_field="Email",
+                                      password_field="Passwd",
+                                      disp=disp)
+
+        self.throttle = 1
+
+    def _verify(self, resp):
+        """
+        Called by the parent class to verify that we have successfully logged
+        in. If the login is unable to be verified a `UnverifiedLoginException`
+        will be raised.
+        """
         # After loging into Google from url_authenticate, I am directed to
         # my account settings page at
         landing_page = 'https://myaccount.google.com/'
 
         if not resp.url.startswith(landing_page):
-            raise Exception("Could not verify that you have successfully logged into Google.")
-
-def login(username, password, disp=False):
-    """
-    Returns a GoogleLogin based on username and password.
-    """
-
-    glogin = GoogleLogin(username,
-                         password,
-                         url_authenticate='https://accounts.google.com/accounts/ServiceLoginAuth',
-                         url_login='https://accounts.google.com/ServiceLogin',
-                         select_form="@id='gaia_loginform'",
-                         username_field="Email",
-                         password_field="Passwd",
-                         disp=disp)
-
-    glogin.throttle = 1
-
-    return glogin
+            raise UnverifiedLoginException()
---
+++ tests/tests.py
@@ -0,0 +1,16 @@
+import google
+
+
+"""
+Teset google account
+First name: Python
+Last name: Package
+email: robsmseghthwne@gmail.com
+password: anog0j4h
+Birthday: April 02 1985
+Gender: Male
+Location: Australia
+"""
+
+def test_google_login():
+    account = google.Session("robsmseghthwne@gmail.com", "anog0j4h")
